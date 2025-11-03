66dfd3a5a4c3770e75da8e3fde38c417654b1ce3
Soldi-sudo <daniilbashkirtsev@gmail.com>
2025-11-04 03:41:16 +0500

    Update fly_script.lua

--- fly_script.lua
+++ fly_script.lua
@@ -1,10 +1,1 @@
--- Fly Script by YourChannelName
--- Активация: Z (летать), X (сбросить скорость), C (увеличить скорость), V (уменьшить скорость)
 
-loadstring(game:HttpGet('https://raw.githubusercontent.com/IsThisMe01/Project-Madara/refs/heads/main/loader.lua'))();
-
--- Дождись загрузки основной библиотеки и затем выполняем Fly
-repeat wait() until _G.Madara
-
--- Активируем функцию полета
-_G.Madara.GetLibrary("Fly").Toggle()
